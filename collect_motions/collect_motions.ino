79aa9ffa7b78901b1df57b6dffedcb95756f75ff
tebashi <tebasaki.wejobs@gmail.com>
2022-07-19 11:51:57 +0900

    #1 add internal imu

--- collect_motions/collect_motions.ino
+++ collect_motions/collect_motions.ino
@@ -13,11 +13,17 @@
 
 ClosedCube::Wired::TCA9548A tca9548a;
 
+// 加速度のみ使用
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
+
 void setup()
 {
   M5.begin();
   Wire.begin();
-  tca9548a.address(PaHub_I2C_ADDRESS);  //Set the I2C address.  设置I2C地址
+  M5.IMU.Init();
+  tca9548a.address(PaHub_I2C_ADDRESS);
   M5.Lcd.setTextFont(4);
   M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
 }
@@ -26,6 +32,8 @@ void loop()
 {
   uint8_t returnCode = 0;
   uint8_t address;
+  
+  // Hubから取得
   for( uint8_t channel=0; channel<4; channel++ ) {
     M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*channel , FRONT);
     M5.Lcd.printf("                                                                ");
@@ -42,6 +50,15 @@ void loop()
         }
       }
     }
-    delay(200);
+    delay(1);
   }
+  
+  // 内臓IMUから取得
+  M5.IMU.getAccelData(&accX,&accY,&accZ);
+  M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*4 , FRONT);
+  M5.Lcd.printf("                                                                ");
+  M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*4 , FRONT);
+  Serial.print("Internal IMU = ");
+  M5.Lcd.printf("Internal %5.2f %5.2f %5.2f", accX, accY, accZ);
+  delay(1);
 }
