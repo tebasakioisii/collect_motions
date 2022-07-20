c992d0617f8889cdb07b52d478165ab33f767654
tebashi <tebasaki.wejobs@gmail.com>
2022-07-20 13:42:54 +0900

    #1 add xilinx uartlite ip

--- collect_motions/collect_motions.ino
+++ collect_motions/collect_motions.ino
@@ -21,38 +21,13 @@ float accZ = 0.0F;
 void setup()
 {
   M5.begin();
-  Wire.begin();
   M5.IMU.Init();
-  tca9548a.address(PaHub_I2C_ADDRESS);
   M5.Lcd.setTextFont(4);
   M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
 }
 
 void loop()
 {
-  uint8_t returnCode = 0;
-  uint8_t address;
-  
-  // Hubから取得
-  for( uint8_t channel=0; channel<4; channel++ ) {
-    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*channel , FRONT);
-    M5.Lcd.printf("                                                                ");
-    M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*channel , FRONT);
-    M5.Lcd.printf("CH%d : ",channel);
-    returnCode = tca9548a.selectChannel(channel);
-    if( returnCode == 0 ) {
-      for(address = 0x01; address < 0x7F; address++ ) {
-        Wire.beginTransmission(address);
-        returnCode = Wire.endTransmission();
-        if (returnCode == 0) {
-          Serial.print("I2C device = ");
-          M5.Lcd.printf("0X%X  ",address);
-        }
-      }
-    }
-    delay(1);
-  }
-  
   // 内臓IMUから取得
   M5.IMU.getAccelData(&accX,&accY,&accZ);
   M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET*4 , FRONT);
