1ca1d46ff2f81deeafbfc55080f95b76ea453821
kawasaki shintaro <kawasaki.shintaro.ks@lila.cs.tsukuba.ac.jp>
2022-09-08 18:14:04 +0900

    #1 send float motion data via uart

--- collect_motions/collect_motions.ino
+++ collect_motions/collect_motions.ino
@@ -13,26 +13,39 @@ float accY = 0.0F;
 float accZ = 0.0F;
 char buf[150];
 
+typedef union {
+  float val;
+  byte bytes[4];
+} f2b;
+
 void setup()
 {
   M5.begin();
   M5.IMU.Init();
   M5.Lcd.setTextFont(4);
   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-  Serial.begin( 57600, SERIAL_8N1, 16, 17 );
+  Serial.begin( 57600, SERIAL_8N1, 13, 14 );
+
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  sprintf(buf, " %5.3f   %5.3f   %5.3f   ", accX, accY, accZ);
+  M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 1 , FRONT);
+  M5.Lcd.printf(buf);
 }
 
 void loop()
 {
-  // 内臓IMUから取得
-  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  f2b fb;
+  fb.val = accX;
+  Serial.write(fb.bytes, 4);
+  M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 2 , FRONT);
+  M5.Lcd.printf("%x %x %x %x", fb.bytes[0], fb.bytes[1], fb.bytes[2], fb.bytes[3]);
+  fb.val = accY;
+  Serial.write(fb.bytes, 4);
+  M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 3 , FRONT);
+  M5.Lcd.printf("%x %x %x %x", fb.bytes[0], fb.bytes[1], fb.bytes[2], fb.bytes[3]);
+  fb.val = accZ;
+  Serial.write(fb.bytes, 4);
   M5.Lcd.setCursor(X_LOCAL, Y_LOCAL + Y_OFFSET * 4 , FRONT);
-  sprintf(buf, " %5.3f   %5.3f   %5.3f   ", accX, accY, accZ);
-  Serial.print(buf);
-  // Serial.write(accX);
-  // Serial.write(accY);
-  // Serial.write(accZ);
-
-  M5.Lcd.printf(buf);
+  M5.Lcd.printf("%x %x %x %x", fb.bytes[0], fb.bytes[1], fb.bytes[2], fb.bytes[3]);
   delay(500);
 }
