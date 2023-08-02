fa4a022bd784acfa2b52ccc5ff9b0736ffd169a7
tebii <tebasaki.wejobs@gmail.com>
2023-08-02 09:16:43 +0900

    シリアルからデータを所得

--- summarize_motion/sum.py
+++ summarize_motion/sum.py
@@ -2,24 +2,24 @@ import serial
 import csv
 import time
 
+s = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+time.sleep(1)
 s = serial.Serial('/dev/ttyACM0', 115200)
 
-# s.reset_output_buffer()
-# s.reset_input_buffer()
-s.write(b'aa\n')
-b = s.readline()
-print(b)
+s.reset_output_buffer()
+s.reset_input_buffer()
+s.write(b'aa')
 time.sleep(1)
 s.write(b'0')
-print(s.readline())
-
 time.sleep(2)
+s.readline()
 
+d = []
 with open('motion1.csv', 'w') as f:
     writer = csv.writer(f)
-    # print(s.read_until(size=100).decode('utf-8'))
     for i in range(10):
-        value = s.readline().decode('utf-8')
-        print(value)
-        writer.writerow(value)
+        value = s.readline().decode('utf-8').rstrip('\r\n')
+        d.append(value)
+        print(value, file=f)
     f.close
+print(d)
