1488b6dcbb9821509f1680ccc7d6565a79a95770
tebii <tebasaki.wejobs@gmail.com>
2023-08-02 09:23:52 +0900

    convert to function

--- summarize_motion/sum.py
+++ summarize_motion/sum.py
@@ -1,25 +1,34 @@
 import serial
 import csv
 import time
+import threading
 
-s = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
-time.sleep(1)
-s = serial.Serial('/dev/ttyACM0', 115200)
 
-s.reset_output_buffer()
-s.reset_input_buffer()
-s.write(b'aa')
-time.sleep(1)
-s.write(b'0')
-time.sleep(2)
-s.readline()
+def get_motion(port = '/dev/ttyACM0'):
+    s = serial.Serial(port, 115200, timeout=0.1)
+    time.sleep(1)
+    s = serial.Serial(port, 115200)
 
-d = []
-with open('motion1.csv', 'w') as f:
-    writer = csv.writer(f)
-    for i in range(10):
-        value = s.readline().decode('utf-8').rstrip('\r\n')
-        d.append(value)
-        print(value, file=f)
-    f.close
-print(d)
+    s.reset_output_buffer()
+    s.reset_input_buffer()
+    s.write(b'aa')
+    time.sleep(1)
+    s.write(b'0')
+    time.sleep(2)
+    s.readline()
+
+    d = []
+    with open('motion1.csv', 'w') as f:
+        writer = csv.writer(f)
+        for i in range(10):
+            value = s.readline().decode('utf-8').rstrip('\r\n')
+            d.append(value)
+            print(value, file=f)
+        f.close
+    print(d)
+
+def main():
+    get_motion()
+
+if __name__ == '__main__':
+    main()
